360f8ba333fe27494e4c1ff1a53aa5162876f8c8
Brad Dunbar <dunbarb2@gmail.com>
2015-07-02 00:58:27 -0400

    Reboot.

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var express = require('express')
 
 var ozymandias = module.exports = function () {
-  var app = express()
-
-  app.use(require('compression'))
-  app.use(express.static('public'))
-  app.use(require('./helpers'))
-
-  return app
+  return express()
 }
 
 for (var p in express) ozymandias[p] = express[p]
