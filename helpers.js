d2e59f914f352bbdc7d43a08db7de286ef8691a9
Brad Dunbar <dunbarb2@gmail.com>
2015-08-30 20:12:51 -0400

    req.permit

--- helpers.js
+++ helpers.js
@@ -1,4 +1,18 @@
+'use strict'
+
 module.exports = function (req, res, next) {
+
+  // Attach specific params.
+  req.permit = function () {
+    let result = {}
+    for (let key of arguments) {
+      result[key] = this.body[key]
+    }
+    return result
+  }
+
+  // Put the request in the locals for convenience.
   res.locals.req = req
+
   next()
 }
--- test/helpers.js
+++ test/helpers.js
@@ -1,11 +1,22 @@
-var test = require('tape')
-var helpers = require('../helpers')
+'use strict'
+
+let test = require('tape')
+let helpers = require('../helpers')
 
 test('locals.req === req', function (t) {
-  var req = {}
-  var res = {locals: {}}
+  let req = {}
+  let res = {locals: {}}
   helpers(req, res, function () {
     t.is(res.locals.req, req)
+    t.end()
+  })
+})
+
+test('req.permit', function (t) {
+  let req = {body: {id: 1, name: 'test', count: 25}}
+  let res = {locals: {}}
+  helpers(req, res, function () {
+    t.deepEqual(req.permit('name', 'count'), {name: 'test', count: 25})
     t.end()
   })
 })
