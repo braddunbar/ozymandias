b7fc89aac04ad4719aafe80fdc07b206336e7dc9
Brad Dunbar <dunbarb2@gmail.com>
2015-09-17 08:17:29 -0400

    Upgrade sql.

--- db/sql.js
+++ db/sql.js
@@ -1,29 +1,7 @@
 'use strict'
 
-let Table = require('sql/lib/table')
-let Query = require('sql/lib/node/query')
 let Postgres = require('sql/lib/dialect/postgres')
 let Parameter = require('sql/lib/node/parameter')
-
-// Fill in some missing table methods.
-for (let method of ['limit', 'offset', 'order']) {
-  Table.prototype[method] = function () {
-    let query = new Query(this)
-    query = query[method].apply(query, arguments)
-    return query
-  }
-}
-
-// Visit queries as subqueries when appropriate.
-let visitQuery = Postgres.prototype.visitQuery
-Postgres.prototype.visitQuery = function (node) {
-  if (this._queryNode) return this.visitSubquery(node)
-  try {
-    return visitQuery.call(this, node)
-  } finally {
-    this._queryNode = null
-  }
-}
 
 let visit = Postgres.prototype.visit
 Postgres.prototype.visit = function (node) {
