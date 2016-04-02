bce477b7c6a823aabf54dbf4ad53ce481879253d
Brad Dunbar <dunbarb2@gmail.com>
2016-04-02 13:15:36 -0400

    Assets version.

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,15 @@
 'use strict'
 
+const crypto = require('crypto')
+
 if (process.env.NODE_ENV === 'production') {
   const assets = require('./fingerprints.json')
   exports.path = (path) => assets[path] || `/${path}`
+  exports.version = crypto
+    .createHash('md5')
+    .update(JSON.stringify(assets))
+    .digest('hex')
 } else {
   exports.path = (path) => `/${path}`
+  exports.version = '☃'
 }
