f36c11f27bf7f78154477c446cb0e76a5704de20
Brad Dunbar <dunbarb2@gmail.com>
2016-05-15 08:33:01 -0400

    Some clean up.

--- test.js
+++ test.js
@@ -4,18 +4,18 @@ const db = require('./db/instance')
 const tape = require('tape')
 const query = db.query
 
-exports = module.exports = function (name, test) {
+module.exports = function (name, test) {
   tape(name, (t) => {
     // Set up transactions.
     const transaction = db.transaction()
     db.query = transaction.query.bind(transaction)
 
     // Rollback the transaction before ending the test.
-    const end = t.end
-    t.end = function () {
-      const args = arguments
-      const next = () => end.apply(t, args)
-      transaction.rollback().then(next).catch(next)
+    const end = t.end.bind(t)
+    t.end = (...args) => {
+      transaction.rollback()
+        .then(() => end(...args))
+        .catch(() => end(...args))
       db.query = query
     }
 
