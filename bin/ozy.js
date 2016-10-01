6e69ab6fb4cfd011dac95b1f376623ace3ac74de
Brad Dunbar <dunbarb2@gmail.com>
2016-10-01 08:02:10 -0400

    Browserify! Uglify!

--- bin/ozy.js
+++ bin/ozy.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const {spawn} = require('child_process')
+const {Readable} = require('stream')
+
 switch (process.argv[2]) {
 
   case 'manifest':
@@ -8,6 +11,26 @@ switch (process.argv[2]) {
 
   case 'rollup':
     require('./rollup')(process.argv[3]).then((code) => console.log(code))
+    break
+
+  case 'browserify':
+    require('./rollup')(process.argv[3]).then((code) => {
+      const input = new Readable()
+
+      const browserify = spawn('browserify', ['-'])
+      input.pipe(browserify.stdin)
+
+      if (process.env.NODE_ENV === 'production') {
+        const uglify = spawn('uglifyjs', ['-m', '-c', 'warnings=false'])
+        browserify.stdout.pipe(uglify.stdin)
+        uglify.stdout.pipe(process.stdout)
+      } else {
+        browserify.stdout.pipe(process.stdout)
+      }
+
+      input.push(code)
+      input.push(null)
+    })
     break
 
   case 'sass':
