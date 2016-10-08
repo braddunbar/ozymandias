c6f17aad080dcdbdfa86d47669c70c499fd22b8e
Brad Dunbar <dunbarb2@gmail.com>
2016-10-07 23:15:34 -0400

    No .env in production.

--- bin/ozy.js
+++ bin/ozy.js
@@ -5,5 +5,10 @@ const {execSync} = require('child_process')
 
 const args = process.argv.slice(3)
 const file = path.join(__dirname, `ozy-${process.argv[2]}`)
+let command = `${file} ${args.join(' ')}`
 
-execSync(`env $(cat .env | xargs) ${file} ${args.join(' ')}`, {stdio: 'inherit'})
+if (process.env.NODE_ENV !== 'production') {
+  command = `env $(cat .env | xargs) ${command}`
+}
+
+execSync(command, {stdio: 'inherit'})
