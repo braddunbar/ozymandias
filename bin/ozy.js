2ca1680561e4481df05a2c6e2f9fe7f945f580d1
Brad Dunbar <dunbarb2@gmail.com>
2016-10-07 23:05:47 -0400

    ozy commands

--- bin/ozy.js
+++ bin/ozy.js
@@ -1,40 +1,9 @@
 #!/usr/bin/env node
 
-const {spawn} = require('child_process')
-const {Readable} = require('stream')
+const path = require('path')
+const {execSync} = require('child_process')
 
-switch (process.argv[2]) {
+const args = process.argv.slice(3)
+const file = path.join(__dirname, `ozy-${process.argv[2]}`)
 
-  case 'manifest':
-    require('../manifest')('public')
-    break
-
-  case 'rollup':
-    require('./rollup')(process.argv[3]).then((code) => console.log(code))
-    break
-
-  case 'browserify':
-    require('./rollup')(process.argv[3]).then((code) => {
-      const input = new Readable()
-
-      const browserify = spawn('browserify', ['-'])
-      input.pipe(browserify.stdin)
-
-      if (process.env.NODE_ENV === 'production') {
-        const uglify = spawn('uglifyjs', ['-m', '-c', 'warnings=false'])
-        browserify.stdout.pipe(uglify.stdin)
-        uglify.stdout.pipe(process.stdout)
-      } else {
-        browserify.stdout.pipe(process.stdout)
-      }
-
-      input.push(code)
-      input.push(null)
-    })
-    break
-
-  case 'sass':
-    console.log(require('./sass')(process.argv[3]))
-    break
-
-}
+execSync(`env $(cat .env | xargs) ${file} ${args.join(' ')}`, {stdio: 'inherit'})
