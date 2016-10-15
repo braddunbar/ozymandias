3483a6328f6fb3558ace0a30c684a032b9db7091
Brad Dunbar <dunbarb2@gmail.com>
2016-10-14 23:14:54 -0400

    request -> https

--- assets/proxy.js
+++ assets/proxy.js
@@ -1,13 +1,18 @@
 'use strict'
 
-const {BUCKET} = process.env
-const request = require('request')
+const https = require('https')
 const {get} = require('koa-route')
 
+const fetch = (url) => new Promise((resolve, reject) => {
+  https.get(url, resolve).on('error', reject)
+})
+
 module.exports = get('/assets/*', function *(next) {
-  if (BUCKET) {
-    this.body = request(`https://s3.amazonaws.com/${BUCKET}${this.url}`)
-  } else {
+  if (!process.env.BUCKET) {
     this.status = 404
+    return
   }
+
+  const path = process.env.BUCKET + this.url.replace(/^\/assets/, '')
+  this.body = yield fetch(`https://s3.amazonaws.com/${path}`)
 })
