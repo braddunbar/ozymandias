3b2529d7a67d3f7ba91ca72b964b72eedb42dab1
Brad Dunbar <dunbarb2@gmail.com>
2017-03-25 11:37:53 -0400

    send better bugsnag errors

--- errors.js
+++ errors.js
@@ -6,14 +6,21 @@ module.exports = async (_, next) => {
   try {
     await next()
   } catch (error) {
+    // Is this a validation error?
     if (error.message === 'invalid' && error.model) {
       _.status = 422
       _.body = error.model.errors
-    } else {
-      console.log(error.stack)
-      bugsnag.notify(error)
-      _.status = 500
-      _.react()
+      return
     }
+
+    // Notify bugsnag
+    bugsnag.notify(error, {
+      req: _.req,
+      user: _.currentUser && _.currentUser.slice('id')
+    })
+
+    // Send back a 500
+    _.status = 500
+    _.react()
   }
 }
