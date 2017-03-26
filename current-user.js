b325accb688d75473fd8fbdd088ae3fdf1e3e857
Brad Dunbar <dunbarb2@gmail.com>
2017-03-26 07:43:52 -0400

    clean up

--- current-user.js
+++ current-user.js
@@ -1,9 +1,10 @@
 'use strict'
 
 module.exports = async (_, next) => {
+  const {client} = _.state
   const id = _.session.userId
   const user = id ? await _.User.find(id) : null
-  _.state.currentUser = _.state.client.currentUser = user
-  _.state.admin = _.state.client.admin = !!(user && user.isAdmin)
+  _.state.currentUser = client.currentUser = user
+  _.state.admin = client.admin = !!(user && user.isAdmin)
   await next()
 }
