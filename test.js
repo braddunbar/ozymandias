5d786769b6e9eb5948b58ac17856a5b2a95f9516
Brad Dunbar <dunbarb2@gmail.com>
2017-05-06 12:23:35 -0400

    t -> assert

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ const db = require('./db/instance')
 const tape = require('tape')
 const Client = require('test-client')
 
-module.exports = (name, test) => tape(name, async (t) => {
-  const app = t.app = ozy()
+module.exports = (name, test) => tape(name, async (assert) => {
+  const app = assert.app = ozy()
 
   for (const route of require('./session')) app.use(route)
 
@@ -20,10 +20,10 @@ module.exports = (name, test) => tape(name, async (t) => {
   const client = new Client(app)
 
   try {
-    await test(t, {app, client})
-    t.end()
+    await test(assert, {app, client})
+    assert.end()
   } catch (error) {
-    t.end(error)
+    assert.end(error)
   } finally {
     transaction.rollback()
   }
