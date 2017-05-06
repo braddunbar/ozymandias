58ecc9e5740db2bbe5d842ad9e8144f9e4dd1762
Brad Dunbar <dunbarb2@gmail.com>
2017-05-06 12:43:52 -0400

    remove assert.app

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const tape = require('tape')
 const Client = require('test-client')
 
 module.exports = (name, test) => tape(name, async (assert) => {
-  const app = assert.app = ozy()
+  const app = ozy()
 
   for (const route of require('./session')) app.use(route)
 
