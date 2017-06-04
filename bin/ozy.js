a8a20bcf180bdd4908a6590657f60ad488e5ff35
Brad Dunbar <dunbarb2@gmail.com>
2017-06-04 11:08:27 -0400

    simplify

--- bin/ozy.js
+++ bin/ozy.js
@@ -9,16 +9,8 @@ const args = process.argv.slice(3)
 const file = path.join(__dirname, `ozy-${process.argv[2]}`)
 let command = `${file} ${args.join(' ')}`
 
-switch (process.env.NODE_ENV) {
-  case 'production':
-    break
-
-  case 'test':
-    break
-
-  default:
-    command = `env $(cat .env | xargs) ${command}`
-    break
+if (!['production', 'test'].includes(process.env.NODE_ENV)) {
+  command = `env $(cat .env | xargs) ${command}`
 }
 
 try {
