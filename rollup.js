0abddf69c6e142bcbb9c7e1fad1abbeb470b371b
Brad Dunbar <dunbarb2@gmail.com>
2017-07-29 13:10:41 -0400

    rollup tests

---
+++ rollup.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const path = require('path')
+const util = require('util')
+const resolve = require('resolve')
+const {rollup} = require('rollup')
+const buble = require('rollup-plugin-buble')
+const {digestPath} = require('./assets')
+const {STATIC_ORIGIN} = process.env
+
+// Insert assets into rollup bundle directly.
+const assets = {
+
+  name: 'assets',
+
+  load (id) {
+    const file = path.relative(process.cwd(), id)
+    if (!/^public\//.test(file)) return null
+    const url = digestPath(path.relative('public', file))
+    const code = `export default ${util.inspect((STATIC_ORIGIN || '') + '/' + url)}`
+
+    const ast = {
+      body: [],
+      end: null,
+      sourceType: 'module',
+      start: 0,
+      type: 'Program'
+    }
+
+    return {ast, code, map: {mappings: ''}}
+  },
+
+  resolveId (importee, importer) {
+    if (/^public\//.test(importee)) {
+      return path.resolve(importee)
+    }
+
+    if (/^ozymandias\//.test(importee)) {
+      return resolve.sync(importee, {basedir: process.cwd()})
+    }
+  }
+
+}
+
+// Insert environment variables into scripts.
+const env = {
+  name: 'env',
+
+  load (id) {
+    const file = path.relative(process.cwd(), id)
+    if (!/^env\//.test(file)) return null
+    const value = process.env[file.slice(4)]
+    const code = `export default ${util.inspect(value)}`
+
+    const ast = {
+      body: [],
+      end: null,
+      sourceType: 'module',
+      start: 0,
+      type: 'Program'
+    }
+
+    return {ast, code, map: {mappings: ''}}
+  },
+
+  resolveId (importee, importer) {
+    if (/^env\//.test(importee)) {
+      return path.resolve(importee)
+    }
+  }
+
+}
+
+module.exports = async (file) => {
+  const code = await rollup({
+    entry: file,
+    onwarn: () => {},
+    plugins: [buble({objectAssign: "require('object-assign')"}), assets, env]
+  })
+  return code.generate({format: 'cjs'}).code
+}
---
+++ test/rollup.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const fs = require('fs')
+const test = require('./test')
+const mkdirp = require('mkdirp')
+const rimraf = require('rimraf')
+const rollup = require('../rollup')
+
+test('setup', async () => {
+  mkdirp.sync('tmp/test')
+  mkdirp.sync('public')
+  process.env.TEST = 'x'
+})
+
+test('rollup some files', async ({assert}) => {
+  fs.writeFileSync('tmp/test/a.js', 'export const x = 1')
+  fs.writeFileSync('tmp/test/b.js', "import {x} from './a'; console.log(x)")
+  assert.is(await rollup('tmp/test/b.js'), "'use strict';\n\nvar x = 1;\n\nconsole.log(x);\n")
+})
+
+test('rollup with environment variables', async ({assert}) => {
+  fs.writeFileSync('tmp/test/a.js', "import value from 'env/TEST'; console.log(value)")
+  assert.is(await rollup('tmp/test/a.js'), "'use strict';\n\nvar value = 'x';\n\nconsole.log(value);\n")
+})
+
+test('rollup with public assets', async ({assert}) => {
+  fs.writeFileSync('tmp/test/a.js', "import path from 'public/asset.txt'; console.log(path)")
+  fs.writeFileSync('public/asset.txt', '1')
+  assert.is(await rollup('tmp/test/a.js'), "'use strict';\n\nvar path = '/assets/asset-6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b.txt';\n\nconsole.log(path);\n")
+})
+
+test('teardown', async () => {
+  rimraf.sync('tmp/test')
+  rimraf.sync('public')
+  delete process.env.TEST
+})
