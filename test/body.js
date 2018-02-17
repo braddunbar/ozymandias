078c9b529186a81ff26a0778c22db45fcf4b646e
Brad Dunbar <dunbarb2@gmail.com>
2018-02-17 10:40:23 -0500

    Strengthen body parsing assertion.

--- test/body.js
+++ test/body.js
@@ -4,11 +4,13 @@ const test = require('./test')
 const {post} = require('koa-route')
 
 test('parse json bodies', async ({assert, app, client}) => {
+  const values = {x: 1, y: 2, z: 3}
+
   app.use(post('/json', async (_) => {
+    assert.deepEqual(_.request.body, values)
     _.body = _.request.body
   }))
 
-  const values = {x: 1, y: 2, z: 3}
   const response = await client
     .post('/json')
     .set('content-type', 'application/json')
